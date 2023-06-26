5f1f82dc7a25fd591e54cb09468f13bd3c98f4be
Lucho808 <amicucci.lucho@gmail.com>
2023-06-26 20:25:39 -0300

    Manejo de archivos en Javascript

--- ProductManager.js
+++ ProductManager.js
@@ -1,79 +1,113 @@
-class Product {
-  static lastId = 122;
+const fs = require('fs');
 
-  constructor(title, description, price, thumbnail, stock) {
-    this.id = ++Product.lastId;
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    this.thumbnail = thumbnail;
-    this.stock = stock;
-    this.code = this.generateCode();
+class ProductManager {
+  constructor(filePath) {
+    this.path = filePath;
   }
 
-  generateCode() {
-    return `abc${this.id}`;
-  }
-}
-
-class ProductList {
-  constructor() {
-    this.products = [];
-  }
-
-  addProduct(title, description, price, thumbnail, stock) {
-    if (!title || !description || !price || !thumbnail || !stock) {
-      console.log("Todos los campos son obligatorios");
-      return;
-    }
-
-    const code = this.generateCode();
-    const codeExists = this.products.some((product) => product.code === code);
-    if (codeExists) {
-      console.log(`Ya existe un producto con el código ${code}`);
-      return;
-    }
-
-    const product = new Product(title, description, price, thumbnail, stock);
-    this.products.push(product);
-    console.log("Producto agregado:", product);
+  addProduct(product) {
+    const products = this.getProductsFromFile();
+    product.id = this.getNextProductId(products);
+    products.push(product);
+    this.saveProductsToFile(products);
+    console.log('Producto agregado: ', product);
   }
 
   getProducts() {
-    return this.products;
+    return this.getProductsFromFile();
   }
 
   getProductById(id) {
-    const product = this.products.find((product) => product.id === id);
+    const products = this.getProductsFromFile();
+    const product = products.find((product) => product.id === id);
     if (product) {
       return product;
     } else {
-      console.error("Not found");
+      console.error('Producto no encontrado');
       return null;
     }
   }
 
-  generateCode() {
-    const lastProduct = this.products[this.products.length - 1];
-    if (lastProduct) {
-      const lastId = lastProduct.id;
-      return `abc${lastId + 1}`;
+  updateProduct(id, updatedFields) {
+    const products = this.getProductsFromFile();
+    const productIndex = products.findIndex((product) => product.id === id);
+    if (productIndex !== -1) {
+      products[productIndex] = { ...products[productIndex], ...updatedFields };
+      this.saveProductsToFile(products);
+      console.log('Producto actualizado: ', products[productIndex]);
     } else {
-      return "abc123";
+      console.error('Producto no encontrado');
     }
+  }
+
+  deleteProduct(id) {
+    const products = this.getProductsFromFile();
+    const productIndex = products.findIndex((product) => product.id === id);
+    if (productIndex !== -1) {
+      const deletedProduct = products.splice(productIndex, 1);
+      this.saveProductsToFile(products);
+      console.log('Producto eliminado: ', deletedProduct[0]);
+    } else {
+      console.error('Producto no encontrado');
+    }
+  }
+
+  getProductsFromFile() {     //leer el JSON que contiene los productos y los devuelve como array de objetos
+    try {
+      const fileContent = fs.readFileSync(this.path, 'utf-8');
+      return JSON.parse(fileContent);
+    } catch (error) {
+      console.error('Error al leer el archivo: ', error);
+      return [];
+    }
+  }
+
+  saveProductsToFile(products) {  //guarda los productos en el JSON
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(products, null, 2));
+      console.log('Productos guardados en el archivo:', this.path);
+    } catch (error) {
+      console.error('Error al guardar los productos: ', error);
+    }
+  }
+
+  getNextProductId(products) {
+    const lastProductId = products.length > 0 ? products[products.length - 1].id : 0;
+    return lastProductId + 1;
   }
 }
 
-const productList = new ProductList();
-productList.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", 25);
-/* productList.addProduct("Auto de carreras", "juguete", 2000, "https://picsum.photos/200/200/?random", 50);
-productList.addProduct("Pelota de futbol", "deporte", 2000, "https://picsum.photos/200/200/?random", 50);
-productList.addProduct("Camiseta", "ropa", 500, "https://picsum.photos/200/200/?random", 20); */
+// Uso de la clase ProductManager
+const productManager = new ProductManager('productos.json');
 
-console.log("Lista de productos: ", productList.getProducts());
+const prueba1 = {
+  title: 'producto prueba1',
+  description: 'Este es un producto prueba',
+  price: 200,
+  thumbnail: 'Sin imagen',
+  stock: 25
+};
 
-const product1 = productList.getProductById(123);
-console.log("Producto con ID 1: ", product1);
+const prueba2 = {
+  title: 'producto prueba2',
+  description: 'Este es otro producto prueba',
+  price: 200,
+  thumbnail: 'Sin imagen',
+  stock: 25
+};
 
-const product2 = productList.getProductById(7);
-console.log("Producto con ID 7: ", product2);
+productManager.addProduct(prueba1);
+productManager.addProduct(prueba2);
+
+productManager.deleteProduct(2);
+
+
+console.log('Lista de productos: ', productManager.getProducts());
+
+const product1 = productManager.getProductById(1);
+console.log('Producto con ID 1: ', product1);
+
+const product2 = productManager.getProductById(2);
+console.log('Producto con ID 2: ', product2);
+
+console.log('Lista de productos después de eliminar: ', productManager.getProducts());
